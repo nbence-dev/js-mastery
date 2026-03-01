7d572f6f86ea605cd73be9535faae78255716f19
Nicholas Bence <nicholasbence12@gmail.com>
2026-03-01 11:33:51 +0200

    implement api routes, server actions and caching and the event details page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database/event.model";
+import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const Page = async () => {
+  "use cache";
+  cacheLife("hours");
   const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
 
--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -7,10 +7,15 @@ export const getSimilarEventsBySlug = async (slug: string) => {
     await connectDB();
 
     const event = await Event.findOne({ slug });
-    return await Event.find({
+    const similarEvent = await Event.find({
       _id: { $ne: event._id },
       tags: { $in: event.tags },
-    }).lean();
+    });
+    return JSON.parse(JSON.stringify(similarEvent));
+    // return await Event.find({
+    //   _id: { $ne: event._id },
+    //   tags: { $in: event.tags },
+    // }).lean();
   } catch (e) {
     return [];
   }
